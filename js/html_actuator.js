02827befd2762f66f90fddf4abd099830d3ad715
William Chern <w.chern.alt@gmail.com>
2019-05-28 09:08:03 -0700

    Modify game-won and game-over messages

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You’ve unlocked Gene Block!" : "Game over";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
